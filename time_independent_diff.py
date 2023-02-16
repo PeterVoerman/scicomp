2f79d304b23d38c2048a5770fa4ed77a5d3e5b41
PeterVoerman <peter.jpv@hotmail.com>
2023-02-16 11:41:29 +0100

    time independent working

--- time_independent_diff.py
+++ time_independent_diff.py
@@ -9,10 +9,15 @@ def jacobi_iteration():
 
 
     grid_list = [np.zeros((N, N)), grid.copy()]
+    counter = 0
+    while max(abs(grid_list[-1] - grid_list[-2]).flatten()) > 1e-4:
+        print(f"{max(abs(grid_list[-1] - grid_list[-2]).flatten()):.7f}", end='\r')
 
-    while max(abs(grid_list[-1] - grid_list[-2]).flatten()) > 1e-6:
         grid[1:-1] = 0.25 * (np.roll(grid, 1, axis=0) + np.roll(grid, -1, axis=0) + np.roll(grid, 1, axis=1) + np.roll(grid, -1, axis=1))[1:-1]
         grid_list.append(grid.copy())
+        counter += 1
+    print()
+    print(counter)
 
     return grid_list[-1]
 
@@ -23,22 +28,53 @@ def gauss_seidel():
     grid[-1] = 1
 
     grid_list = [np.zeros((N, N)), grid.copy()]
-
-    while max(abs(grid_list[-1] - grid_list[-2]).flatten()) > 1e-6:
+    counter = 0
+    while max(abs(grid_list[-1] - grid_list[-2]).flatten()) > 1e-4:
+        print(f"{max(abs(grid_list[-1] - grid_list[-2]).flatten()):.7f}", end='\r')
         new_grid = np.zeros((N, N))
         new_grid[-1] = 1
-        for y in range(1, N-2):
-            new_grid[0, y] = 0.25 * (grid[0, y + 1] + grid[0, y - 1] + grid[1, y] + grid[-1, y])
+        for y in range(1, N-1):
+            new_grid[y][0] = 0.25 * (grid[y + 1][0] + grid[y - 1][0] + grid[y][1] + grid[y][-1])
             for x in range(1, N-1):
-                new_grid[x, y] = 0.25 * (grid[x, y + 1] + new_grid[x, y - 1] + grid[x + 1, y] + new_grid[x - 1, y])
+                new_grid[y][x] = 0.25 * (grid[y + 1][x] + new_grid[y - 1][x] + grid[y][x + 1] + new_grid[y][x - 1])
+            new_grid[y][-1] = 0.25 * (grid[y + 1][-1] + new_grid[y - 1][-1] + grid[y][-2] + new_grid[y][0])
 
-        print(new_grid[0, 1])
         grid = new_grid.copy()
         grid_list.append(grid.copy())
 
-        plt.imshow(grid, origin='lower')
-        plt.show()
+        counter += 1
+    print()
+    print(counter)
+
     return grid_list[-1]
 
-plt.imshow(gauss_seidel(), origin='lower')
-plt.show()
+def successive_over_relaxation(omega):
+    N = 100
+
+    grid = np.zeros((N, N))
+    grid[-1] = 1
+
+    grid_list = [np.zeros((N, N)), grid.copy()]
+    counter = 0
+    while max(abs(grid_list[-1] - grid_list[-2]).flatten()) > 1e-4:
+        print(f"{max(abs(grid_list[-1] - grid_list[-2]).flatten()):.7f}", end='\r')
+        new_grid = np.zeros((N, N))
+        new_grid[-1] = 1
+        for y in range(1, N-1):
+            new_grid[y][0] = 0.25 * (grid[y + 1][0] + grid[y - 1][0] + grid[y][1] + grid[y][-1]) + (1 - omega) * grid[y][0]
+            for x in range(1, N-1):
+                new_grid[y][x] = (1 - omega) * grid[y][x] + omega * 0.25 * (grid[y + 1][x] + new_grid[y - 1][x] + grid[y][x + 1] + new_grid[y][x - 1])
+            new_grid[y][-1] = 0.25 * (grid[y + 1][-1] + new_grid[y - 1][-1] + grid[y][-2] + new_grid[y][0]) + (1 - omega) * grid[y][-1]
+
+        grid = new_grid.copy()
+        grid_list.append(grid.copy())
+
+        counter += 1
+    print()
+    print(counter)
+
+    return grid_list[-1]
+
+jacobi_iteration()
+gauss_seidel()
+successive_over_relaxation(1.7)
