a0a7672c8041f90c876795eb2d345a7b8799d778
leon <leonkuiper.nl@gmail.com>
2023-03-03 10:49:10 +0100

    in mapje

---
+++ assignment1/wave.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import numpy as np
+import math
+plt.rcParams.update({'font.size': 15})
+
+N = 1000
+c_squared = 1
+L = 1
+delta_t = 0.001
+t_max = 1
+delta_x = L/N
+
+boundary1 = 0
+boundary2 = 0
+
+u = [math.sin(2*math.pi*x) for x in np.arange(0,L,delta_x)]
+# u = [math.sin(5*math.pi*x) for x in np.arange(0,L,delta_x)]
+# u = [math.sin(5*math.pi*x) if x >= 1/5 and x <= 2/5 else 0 for x in np.arange(0,L,delta_x)]
+
+u[0] = boundary1
+u[-1] = boundary2
+
+string = [u.copy(),u.copy()]
+
+for dt in np.arange(0,t_max,delta_t):
+    print(f"{dt}/{t_max}", end="\r")
+    u = np.zeros(N)
+    for i in range(1,len(u)-1):
+        u[i] = c_squared * (delta_t**2/delta_x**2)*(string[-1][i+1] + string[-1][i-1]-2*string[-1][i]) - string[-2][i] + 2*string[-1][i]
+    string.append(u)
+
+
+string_x = [x for x in np.arange(0,L,delta_x)]
+# for i in range(0, len(string), 10):
+#     plt.plot(string_x, string[i])
+#     plt.ylim(-1,1)
+#     plt.xlim(0,L)
+#     plt.draw()
+#     plt.pause(0.0001)
+#     plt.clf()
+
+for i in np.arange(0, 1000, 150):
+    plt.plot(string_x,string[i],label=f't={i/1000:.2f}')
+
+plt.ylim(-1,1)
+plt.xlim(0,L)
+plt.xlabel('x')
+plt.ylabel('A')
+plt.legend()
+plt.tight_layout()
+plt.savefig("wave1.png")
