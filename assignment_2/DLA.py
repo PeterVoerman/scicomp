3dcd894b3d911148d6074bec329dae75d4025e14
leon <leonkuiper.nl@gmail.com>
2023-03-07 15:21:28 +0100

    pkl

--- assignment_2/DLA.py
+++ assignment_2/DLA.py
@@ -5,13 +5,18 @@ import pickle
 
 start = time.time()
 
-def successive_over_relaxation(grid, cluster, t, omega = 1, make_pickle = False, pickle_name = ''):
+def successive_over_relaxation(grid, cluster, t, omega = 1, delta_precision = 1e-4, make_pickle = False, pickle_name = ''):
     counter = 0
     delta = 1
+    max_counter = 1e4
+
+    if make_pickle:
+        max_counter = 1e6
 
     N = len(grid)
 
-    while delta > 1e-3 and delta < 1e5 and counter < 1e4:
+    while delta > delta_precision and delta < 1e5 and counter < max_counter:
+        # print(f"counter = {counter}, delta = {delta:.2E}     ", end='\r')s
         new_grid = grid.copy()
         new_grid[-1] = 1
         for y in range(1, N-1):
@@ -27,7 +32,7 @@ def successive_over_relaxation(grid, cluster, t, omega = 1, make_pickle = False,
         counter += 1
 
     if make_pickle:
-        with open(f'{pickle_name}.plk', 'wb') as save_file:
+        with open(f'N{N}delta{delta_precision}.pkl', 'wb') as save_file:
             pickle.dump(grid, save_file)
     
     return grid, counter
@@ -51,7 +56,7 @@ def find_neighbors(cluster, N):
 
     return neighbors
 
-def run_simulation(grid, cluster, eta, growth_steps=100, max_iterations=np.inf):
+def run_simulation(grid, cluster, eta, omega = 1, growth_steps=100, delta_precision = 1e-4, max_iterations=np.inf):
     total_counter = 0
 
     for t in range(growth_steps):
@@ -59,7 +64,7 @@ def run_simulation(grid, cluster, eta, growth_steps=100, max_iterations=np.inf):
         if total_counter > max_iterations:
             break
 
-        grid, counter = successive_over_relaxation(grid, cluster)
+        grid, counter = successive_over_relaxation(grid, cluster, t, omega, delta_precision)
         total_counter += counter
 
         probability_sum = 0
@@ -77,35 +82,57 @@ def run_simulation(grid, cluster, eta, growth_steps=100, max_iterations=np.inf):
 
     return grid, cluster
 
+def make_pickle(N, delta_precision):
+    grid = np.zeros((N,N))
+    grid, counter = successive_over_relaxation(grid, [], 0, omega = 1, delta_precision = delta_precision, make_pickle = True, pickle_name = f'N{N}')
+    return
+
 def make_eta_plot(eta):
 
     N = 100 
-    growth_steps = 750
-    eta = 1
+    growth_steps = 100
+    # eta = 1
+    omega = 1
+    delta_precision = 1e-5
+    delta_precision_pkl = 1e-5
 
-    grid = np.zeros((N,N))
-    grid = np.array([np.array([x/N for y in range(N)]) for x in range(N)])
+    # grid = np.zeros((N,N))
+    # grid = np.array([np.array([x/N for y in range(N)]) for x in range(N)])
 
-    # with open("no_sink_N100.pkl", 'rb') as save_file:
-    # grid = pickle.load(save_file)
+    with open(f"N{N}delta{delta_precision_pkl}.pkl", 'rb') as save_file:
+        grid = pickle.load(save_file)
 
-    grid = np.zeros((N,N))
-    grid = successive_over_relaxation(grid, [], 0, omega = 1, make_pickle = True, pickle_name = f'N{N}')
-    quit()
+    
 
     cluster = [(0, N // 2)]
 
-    grid, cluster = run_simulation(grid, cluster, eta, growth_steps, max_iterations=1e4)
+    grid, cluster = run_simulation(grid, cluster, eta, omega, growth_steps, delta_precision)
     print()
 
     for coord in cluster:
         grid[coord[0]][coord[1]] = None
 
     plt.imshow(grid, origin='lower', cmap='gist_rainbow')
-    plt.savefig(f'eta_{eta}.png')
+    plt.savefig(f'eta_{eta}_t_{growth_steps}.png')
     plt.clf()
 
-for eta in [0, 0.5, 1, 1.5, 2]:
+
+# make starting grid and save in pickle file
+# make_pickle(100, 1e-5)
+# quit()
+
+
+# plot starting grid that is loaded via pickle to check
+# N=100
+# delta_precision = 1e-5
+# with open(f"N{N}delta{delta_precision}.pkl", 'rb') as save_file:
+#     grid = pickle.load(save_file)
+
+# plt.imshow(grid, origin='lower', cmap='gist_rainbow')
+# plt.show()
+# quit()
+
+for eta in [0, 0.5, 1, 1.5, 2, 2.5]:
     print(f"eta = {eta}")
     make_eta_plot(eta)
 
