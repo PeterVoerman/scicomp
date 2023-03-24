82d71f256427cc274e175d10f26fe8dd356508a0
PeterVoerman <peter.jpv@hotmail.com>
2023-03-24 10:06:59 +0100

    plots

--- assignment_3/part1.py
+++ assignment_3/part1.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import linalg
 from scipy import sparse
+import time
 
 from matplotlib import colors
+import matplotlib
 
-import time
+matplotlib.rcParams['font.size'] = 16
 
 def generate_matrix(grid):
     matrix = np.zeros((np.count_nonzero(grid) + 1, np.count_nonzero(grid) + 1))
@@ -51,7 +53,7 @@ def generate_sparse_matrix(grid, mask=None):
                     col.append(grid[y, x - 1])
                     data.append(1)
                     neighbor_count += 1
-                if x + 1 < len(grid) and mask[y][x + 1] == 1:
+                if x + 1 < len(grid[0]) and mask[y][x + 1] == 1:
                     row.append(element)
                     col.append(grid[y, x + 1])
                     data.append(1)
@@ -78,30 +80,210 @@ def generate_sparse_matrix(grid, mask=None):
     return sparse.csr_matrix((data, (row, col)))
     
 
-# Square
-L = 50
-grid = np.arange(0, L ** 2).reshape(L, L)
-
-# start_non_sparse = time.time()
-# matrix = generate_matrix(grid)
-# result = linalg.eig(matrix)
-# end_non_sparse = time.time()
-
-# print("Non-sparse time: ", end_non_sparse - start_non_sparse)
-
-# start_sparse = time.time()
+# # Square
+# L = 100
+# grid = np.arange(0, L ** 2).reshape(L, L)
 # matrix = generate_sparse_matrix(grid)
 # result = sparse.linalg.eigs(matrix)
-# end_sparse = time.time()
 
-# print("Sparse time: ", end_sparse - start_sparse)
+# fig, ax = plt.subplots(2, 2)
+# ax[0, 0].imshow(np.abs(result[1][:, 0]).reshape(L, L))
+# ax[0, 1].imshow(np.abs(result[1][:, 1]).reshape(L, L))
+# ax[1, 0].imshow(np.abs(result[1][:, 2]).reshape(L, L))
+# ax[1, 1].imshow(np.abs(result[1][:, 3]).reshape(L, L))
+# ax[0, 0].set_title(f"Eigenvalue: {np.real(result[0][0]):.4f}")
+# ax[0, 1].set_title(f"Eigenvalue: {np.real(result[0][1]):.4f}")
+# ax[1, 0].set_title(f"Eigenvalue: {np.real(result[0][2]):.4f}")
+# ax[1, 1].set_title(f"Eigenvalue: {np.real(result[0][3]):.4f}")
+# ax[0, 0].set_xticks([])
+# ax[0, 0].set_yticks([])
+# ax[0, 1].set_xticks([])
+# ax[0, 1].set_yticks([])
+# ax[1, 0].set_xticks([])
+# ax[1, 0].set_yticks([])
+# ax[1, 1].set_xticks([])
+# ax[1, 1].set_yticks([])
+# plt.tight_layout()
+# plt.savefig("plots/square.png")
+# plt.clf()
 
-# plt.imshow(np.abs(result[1][:,0]).reshape(L, L))
-# plt.show()
 
 
-# Circle
-L = 50
+# # Circle
+# L = 100
+# grid = np.arange(0, L ** 2).reshape(L, L)
+
+# mask = np.zeros((L, L))
+# for y in range(L):
+#     for x in range(L):
+#         if (x - L / 2) ** 2 + (y - L / 2) ** 2 <= (L / 2) ** 2:
+#             mask[y, x] = 1
+
+
+# matrix = generate_sparse_matrix(grid, mask=mask)
+# result = sparse.linalg.eigs(matrix)
+
+# fig, ax = plt.subplots(2, 2)
+# ax[0, 0].imshow(np.abs(result[1][:, 0]).reshape(L, L))
+# ax[0, 1].imshow(np.abs(result[1][:, 1]).reshape(L, L))
+# ax[1, 0].imshow(np.abs(result[1][:, 2]).reshape(L, L))
+# ax[1, 1].imshow(np.abs(result[1][:, 3]).reshape(L, L))
+# ax[0, 0].set_title(f"Eigenvalue: {np.real(result[0][0]):.4f}")
+# ax[0, 1].set_title(f"Eigenvalue: {np.real(result[0][1]):.4f}")
+# ax[1, 0].set_title(f"Eigenvalue: {np.real(result[0][2]):.4f}")
+# ax[1, 1].set_title(f"Eigenvalue: {np.real(result[0][3]):.4f}")
+# ax[0, 0].set_xticks([])
+# ax[0, 0].set_yticks([])
+# ax[0, 1].set_xticks([])
+# ax[0, 1].set_yticks([])
+# ax[1, 0].set_xticks([])
+# ax[1, 0].set_yticks([])
+# ax[1, 1].set_xticks([])
+# ax[1, 1].set_yticks([])
+# plt.tight_layout()
+# plt.savefig("plots/circle.png")
+# plt.clf()
+
+
+# # Rectangle
+# L1 = 75
+# L2 = 150
+# grid = np.arange(0, L1 * L2).reshape(L1, L2)
+# matrix = generate_sparse_matrix(grid)
+# result = sparse.linalg.eigs(matrix)
+
+# fig, ax = plt.subplots(2, 2)
+# ax[0, 0].imshow(np.abs(result[1][:, 0]).reshape(L1, L2))
+# ax[0, 1].imshow(np.abs(result[1][:, 1]).reshape(L1, L2))
+# ax[1, 0].imshow(np.abs(result[1][:, 2]).reshape(L1, L2))
+# ax[1, 1].imshow(np.abs(result[1][:, 3]).reshape(L1, L2))
+# ax[0, 0].set_title(f"Eigenvalue: {np.real(result[0][0]):.4f}")
+# ax[0, 1].set_title(f"Eigenvalue: {np.real(result[0][1]):.4f}")
+# ax[1, 0].set_title(f"Eigenvalue: {np.real(result[0][2]):.4f}")
+# ax[1, 1].set_title(f"Eigenvalue: {np.real(result[0][3]):.4f}")
+# ax[0, 0].set_xticks([])
+# ax[0, 0].set_yticks([])
+# ax[0, 1].set_xticks([])
+# ax[0, 1].set_yticks([])
+# ax[1, 0].set_xticks([])
+# ax[1, 0].set_yticks([])
+# ax[1, 1].set_xticks([])
+# ax[1, 1].set_yticks([])
+# plt.tight_layout()
+# plt.savefig("plots/rectangle.png")
+# plt.clf()
+
+
+# # Time plot
+# non_sparse_time_list = []
+# sparse_time_list = []
+# x_list_non_sparse = []
+
+# for L in range(5, 105, 5):
+#     print(f"L = {L}", end="\r")
+
+#     grid = np.arange(0, L ** 2).reshape(L, L)
+
+#     if L <= 60:
+#         start_non_sparse = time.time()
+#         matrix = generate_matrix(grid)
+#         result = linalg.eig(matrix)
+#         end_non_sparse = time.time()
+#         x_list_non_sparse.append(L)
+#         non_sparse_time_list.append(end_non_sparse - start_non_sparse)
+
+#     start_sparse = time.time()
+#     matrix = generate_sparse_matrix(grid)
+#     result = sparse.linalg.eigs(matrix)
+#     end_sparse = time.time()
+
+    
+#     sparse_time_list.append(end_sparse - start_sparse)
+
+# plt.plot(x_list_non_sparse, non_sparse_time_list, label="Non-sparse")
+# plt.plot(range(5, 105, 5), sparse_time_list, label="Sparse")
+# plt.legend()
+# plt.xlabel("L")
+# plt.ylabel("Time (s)")
+# plt.xlim(5, 100)
+# plt.ylim(0, max(non_sparse_time_list))
+# plt.tight_layout()
+# plt.savefig("plots/time.png")
+# plt.clf()
+
+
+# # Eigenfrequency plot
+# square_list = []
+# circle_list = []
+# rectangle_list = []
+# L_list = []
+
+# for L in range(10, 105, 5):
+#     L_list.append(L)
+
+#     print(f"L = {L}", end="\r")
+
+#     grid = np.arange(0, L ** 2).reshape(L, L)
+#     matrix = generate_sparse_matrix(grid)
+#     result = sparse.linalg.eigs(matrix)
+
+#     square_list.append(np.real(result[0]))
+
+#     grid = np.arange(0, L ** 2).reshape(L, L)
+#     mask = np.zeros((L, L))
+#     for y in range(L):
+#         for x in range(L):
+#             if (x - L / 2) ** 2 + (y - L / 2) ** 2 <= (L / 2) ** 2:
+#                 mask[y, x] = 1
+    
+#     matrix = generate_sparse_matrix(grid, mask)
+#     result = sparse.linalg.eigs(matrix)
+
+#     circle_list.append(np.real(result[0]))
+
+#     L1 = L
+#     L2 = 2 * L
+#     grid = np.arange(0, L1 * L2).reshape(L1, L2)
+#     matrix = generate_sparse_matrix(grid)
+#     result = sparse.linalg.eigs(matrix)
+
+#     rectangle_list.append(np.real(result[0]))
+
+# counter = 0
+# for freq in square_list:
+#     plt.scatter([L_list[counter]] * len(freq), freq, s=1, c="b")
+#     counter += 1
+
+# plt.xticks(range(0, 105, 20))
+# plt.xlabel("L")
+# plt.ylabel("Eigenfrequency")
+# plt.savefig("plots/square_freqs.png")
+# plt.clf()
+
+# counter = 0
+# for freq in circle_list:
+#     plt.scatter([L_list[counter]] * len(freq), freq, s=1, c="b")
+#     counter += 1
+
+# plt.xticks(range(0, 105, 20))
+# plt.xlabel("L")
+# plt.ylabel("Eigenfrequency")
+# plt.savefig("plots/circle_freqs.png")
+# plt.clf()
+
+# counter = 0
+# for freq in rectangle_list:
+#     plt.scatter([L_list[counter]] * len(freq), freq, s=1, c="b")
+#     counter += 1
+
+# plt.xticks(range(0, 105, 20))
+# plt.xlabel("L")
+# plt.ylabel("Eigenfrequency")
+# plt.savefig("plots/rectangle_freqs.png")
+# plt.clf()
+
+
+L = 100
 grid = np.arange(0, L ** 2).reshape(L, L)
 
 mask = np.zeros((L, L))
@@ -114,20 +296,44 @@ for y in range(L):
 matrix = generate_sparse_matrix(grid, mask=mask)
 result = sparse.linalg.eigs(matrix)
 
-# plt.imshow(np.abs(result[1][:,5]).reshape(L, L))
-# plt.show()
+index = 0
+eigenvector = np.abs(result[1][:, index].reshape(L, L))
+eigenvalue = np.abs(result[0][index])
+divnorm=colors.TwoSlopeNorm(vmin=-np.amax(eigenvector.copy()), vcenter=0., vmax=np.amax(eigenvector.copy()))
+
+for t in np.arange(0, 5, 0.05):
+    u = eigenvector * (np.cos(eigenvalue ** 0.5 * t) + np.sin(eigenvalue ** 0.5 * t))
+    plt.imshow(u, animated=True, norm=divnorm)
+    plt.xticks([])
+    plt.yticks([])
+    plt.draw()
+    plt.pause(0.01)
+    plt.clf()
 
 index = 1
 eigenvector = np.abs(result[1][:, index].reshape(L, L))
 eigenvalue = np.abs(result[0][index])
-
-# print(np.amin(eigenvector), np.amax(eigenvector))
-
 divnorm=colors.TwoSlopeNorm(vmin=-np.amax(eigenvector.copy()), vcenter=0., vmax=np.amax(eigenvector.copy()))
 
-for t in np.arange(0, 10, 0.05):
+for t in np.arange(0, 5, 0.05):
     u = eigenvector * (np.cos(eigenvalue ** 0.5 * t) + np.sin(eigenvalue ** 0.5 * t))
     plt.imshow(u, animated=True, norm=divnorm)
+    plt.xticks([])
+    plt.yticks([])
+    plt.draw()
+    plt.pause(0.01)
+    plt.clf()
+
+index = 5
+eigenvector = np.abs(result[1][:, index].reshape(L, L))
+eigenvalue = np.abs(result[0][index])
+divnorm=colors.TwoSlopeNorm(vmin=-np.amax(eigenvector.copy()), vcenter=0., vmax=np.amax(eigenvector.copy()))
+
+for t in np.arange(0, 5, 0.05):
+    u = eigenvector * (np.cos(eigenvalue ** 0.5 * t) + np.sin(eigenvalue ** 0.5 * t))
+    plt.imshow(u, animated=True, norm=divnorm)
+    plt.xticks([])
+    plt.yticks([])
     plt.draw()
     plt.pause(0.01)
     plt.clf()
